cb949817041142effd6b81ec462adaaf62a85909
YoYag <la2043432@gmail.com>
2022-05-28 14:10:39 +0900

    수정기능 추가

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 import express from "express";
-import {
-  application
-} from "express";
 import mysql from "mysql2/promise";
 import cors from "cors";
 
@@ -24,25 +21,99 @@ const corsOptions = {
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions)); // cors 사용
 
 const port = 3000;
 
-// 랜덤조회GET
-app.get("/wiseSayings", async (req, res) => {
-  const [[rows]] = await pool.query(`
-  SELECT * 
-  FROM wise_saying 
-  ORDER BY RAND() LIMIT 1;
-  `);
+// 수정PATCH
+app.patch("/wiseSaying/:id", async (req, res) => {
+  const { id } = req.params;
+  const [[wiseSayingRow]] = await pool.query(
+    `
+    SELECT *
+    FROM wise_saying
+    WHERE id = ?;
+    `,
+    [id]
+  );
 
-  await pool.query(`
-  UPDATE wise_saying
-  SET hits = hits + 1
+  if (wiseSayingRow === 0) {
+    res.status(404).json({
+      resultCode: "F-1",
+      msg: "404 not found",
+    });
+    return;
+  }
+
+  const {
+    content = wiseSayingRow.content,
+    figure = wiseSayingRow.figure,
+    like_it = wiseSayingRow.like_it,
+    hate_it = wiseSayingRow.hate_it,
+  } = req.body;
+
+  await pool.query(
+    `
+    UPDATE wise_saying
+    SET content = ?,
+    figure = ?,
+    like_it = ?,
+    hate_it = ?
+    WHERE id = ?
+    `,
+    [content, figure, like_it, hate_it, id]
+  );
+
+  const [[justModifiedWiseSayingRow]] = await pool.query(
+    `
+  SELECT *
+  FROM wise_saying
   WHERE id = ?;
-  `, [rows.id]);
+  `,
+    [id]
+  );
 
-  res.json([rows]);
+  res.json({
+    resultCode: "S-1",
+    msg: "성공",
+    data: justModifiedWiseSayingRow,
+  });
+});
+
+// 랜덤조회GET
+app.get("/wiseSaying/random", async (req, res) => {
+  const [[wiseSayingRow]] = await pool.query(
+    `
+    SELECT * 
+    FROM wise_saying 
+    ORDER BY RAND() LIMIT 1;
+    `
+  );
+
+  if (wiseSayingRow === 0) {
+    res.status(404).json({
+      resultCode: "F-1",
+      msg: "404 not found",
+    });
+    return;
+  }
+
+  wiseSayingRow.hits++;
+
+  await pool.query(
+    `
+    UPDATE wise_saying
+    SET hits = ?
+    WHERE id = ?;
+    `,
+    [wiseSayingRow.hits, wiseSayingRow.id]
+  );
+
+  res.json({
+    resultCode: "S-1",
+    msg: "성공",
+    data: wiseSayingRow,
+  });
 });
 
 app.listen(port, () => {
